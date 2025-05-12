185bb5648e9630835107c062166c2df4b2405c4a
Vignesh-Lal <vlal2@csustan.edu>
2025-05-12 09:14:32 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -137,8 +137,8 @@ const CONFIG = {
   ],
   // Display articles from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
+    source: '', // medium | dev
+    username: '', // to hide blog section, keep it empty
     limit: 1, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
